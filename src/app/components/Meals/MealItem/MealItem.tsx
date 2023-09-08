2dd629ef5c1043a8185f82415286072a91dbe3fa
fatmakhv <fatmaba@gmail.com>
2023-09-08 20:45:11 +0300

    Fix

--- src/app/components/Meals/MealItem/MealItem.tsx
+++ src/app/components/Meals/MealItem/MealItem.tsx
@@ -5,14 +5,13 @@ import { FC } from 'react';
 import './MealItem.css';
 
 const MealItem: FC<Meal> = ({ description, id, key, name, price }): JSX.Element => {
-  price = `$${price.toFixed(2)}`;
 
   return (
     <li className="meal">
       <div>
         <h3>{name}</h3>
         <div className="description">{description}</div>
-        <div className="price">{price}</div>
+        <div className="price">${price.toFixed(2)}</div>
         <div>
           
         </div>
